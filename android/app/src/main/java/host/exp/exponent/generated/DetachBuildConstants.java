c236c3667f7f169d7c8baa7a1369158da68be1ca
modulitos <you@example.com>
2019-04-21 19:17:09 -0700

    upgrade gradle

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expae4408fa875446ea8e419080c4bcd8c8://192.168.1.178:80";
 
 }
